8978b998b5063a3972e42ef51006eb2e42d1a070
Stephen Blevins <scblevins27@gmail.com>
2021-12-29 11:52:03 -0500

    moved items from navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import "./css/App.css";
 import React, { useState, useEffect } from "react";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { WatchList } from "./components/WatchList";
 
 function App() {
   const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
+  const [graphData, setGraphData] = useState([]);
+
+  function addCoin(newCoin) {
+    if (!list.includes(newCoin)) {
+      setList((oldList) => [...oldList, newCoin]);
+    }
+  }
+
+  function removeCoin(coin) {
+    setList(list.filter((item) => item.id != coin.id));
+  }
+
+  let newData = [];
+  const getChartData = async (coin) => {
+    let timestamp = Date.now();
+    
+    if (coin != undefined) {
+      const chartUrl = `https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart/range?vs_currency=usd&from=1624407061&to=${timestamp}`;
+      
+      try {
+        const res = await fetch(chartUrl, { mode: "cors" });
+        const data = await res.json(); 
+        data.prices.map((month, index) => {
+          if (index % 30 == 0) {
+            newData.push(month);
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+      setGraphData(newData)
+      console.log(newData)
+    }
+  };
 
   // const url = "https://jsonplaceholder.typicode.com/users";
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
@@ -19,10 +56,25 @@ function App() {
   useEffect(() => {
     getCoinData()
   }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar data={data}/>
+        <Routes>
+        <Route
+          exact path="/CryptoTracker/"
+          element={
+            <Home data={data} add={addCoin} getChartData={getChartData} />
+          }
+        />
+        <Route
+          path="watchlist"
+          element={
+            <WatchList data={list} remove={removeCoin} />
+          }
+        />
+      </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { Home } from "./Home";
-import { WatchList } from "./WatchList";
+import React from "react";
+import { Link } from "react-router-dom";
+
 import "../css/App.css";
 
-export const Navbar = (props) => {
-  const [list, setList] = useState([]);
-  const [graphData, setGraphData] = useState([]);
-
-  function addCoin(newCoin) {
-    if (!list.includes(newCoin)) {
-      setList((oldList) => [...oldList, newCoin]);
-    }
-  }
-
-  function removeCoin(coin) {
-    setList(list.filter((item) => item.id != coin.id));
-  }
-
-  let newData = [];
-  const getChartData = async (coin) => {
-    let timestamp = Date.now();
-    
-    if (coin != undefined) {
-      const chartUrl = `https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart/range?vs_currency=usd&from=1624407061&to=${timestamp}`;
-      
-      try {
-        const res = await fetch(chartUrl, { mode: "cors" });
-        const data = await res.json(); 
-        data.prices.map((month, index) => {
-          if (index % 30 == 0) {
-            newData.push(month);
-          }
-        });
-      } catch (err) {
-        console.log(err);
-      }
-      setGraphData(newData)
-      console.log(newData)
-    }
-  };
-
+export const Navbar = () => {
 
   return (
     <div>
@@ -58,20 +21,7 @@ export const Navbar = (props) => {
           </li>
         </ul>
       </nav>
-      <Routes>
-        <Route
-          exact path="/CryptoTracker/"
-          element={
-            <Home data={props.data} add={addCoin} getChartData={getChartData} />
-          }
-        />
-        <Route
-          path="watchlist"
-          element={
-            <WatchList data={list} remove={removeCoin} />
-          }
-        />
-      </Routes>
+
     </div>
   );
 };
